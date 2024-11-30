3ccf36b2a310176adb1168acb763ca458e91302a
Suchan R <26240553+tncks@users.noreply.github.com>
2024-11-30 22:08:15 +0900

    11 30

---
+++ cminus/error_messages.c
@@ -0,0 +1,9 @@
+
+fprintf(listing, "Error: Invalid array indexing at line %d (name : \"%s\"). indexing can only allowed for int[] variables\n", lineno, name);
+
+
+fprintf(listing, "Error: invalid condition at line %d\n", lineno);
+// if 랑 condition 관련 코드 구현이 안되어있어서 이건 나중에. 적용하고, 5번줄 삭제
+
+
+
